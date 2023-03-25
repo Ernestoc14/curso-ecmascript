ef9d1e7581b0352a844fb6dc8bdafe89fe29ee52
Ernestoc14 <descreed34@hotmail.com>
2023-03-25 00:10:29 -0500

    Let, Var, Const Class

--- 00-let-const.js
+++ 00-let-const.js
@@ -1,2 +1,14 @@
 const animal = 'Dog'
-animal = 'Cat'
+//animal = 'Cat'
+console.log(animal)
+
+const fruits = () => {
+    if (true) {
+        var fruits1 = 'apple' //var is global
+        let fruits2 = 'kiwi' //let is local
+        const fruits3 = 'banana' //const is local
+    }
+    console.log(fruits1)
+    console.log(fruits2)
+    console.log(fruits3)
+}
