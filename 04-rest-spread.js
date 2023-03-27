7f6116e3ba6064d638178d56454ddf526c1d3cc6
Ernestoc14 <descreed34@hotmail.com>
2023-03-27 09:20:29 -0500

    Rest Spread Operator Class

--- 04-rest-spread.js
+++ 04-rest-spread.js
@@ -6,10 +6,24 @@ console.log(a, fruits[1], c)
 //Objets destructuring
 let user = {
     username: 'Ernesto',
-    age: 24,
-    country: 'PTY'
+    age: 24
+}
+    let {username, age} = user;
+    console.log(username, age)   
+
+//Spread operator
+    let person = { name: 'Ernesto', age: 24};
+    let country = 'PTY';
+                                        // We create a new Object using the spread operator 
+                                        // to copy the values of the other objects
+    let data = {...person, country};    // ...person = person.name, person.age
+    console.log(data)                  
+
+//Rest operator
+    function sum(num, ...values) {
+        console.log(values)
+        console.log(num+values[0])
+        return num + values[0]
     }
 
-    let {username, age, country} = user;
-    console.log(username, age, country)
-    
+    sum(1, 2, 3, 4, 5)
