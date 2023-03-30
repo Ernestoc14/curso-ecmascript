789fcf6574cae40204d3571427fff0e345cd8c1a
Ernestoc14 <descreed34@hotmail.com>
2023-03-30 09:36:57 -0500

    Convert Objects to Array

---
+++ ES8-object-entries.js
@@ -0,0 +1,7 @@
+const countries = { 
+    USA: 'United States of America',
+    IND: 'India',   
+    AUS: 'Australia'
+};
+console.log(Object.entries(countries)); // [ [ 'USA', 'United States of America' ], [ 'IND', 'India' ], [ 'AUS', 'Australia' ] ]
+// Object.entries() returns an array of objects 
---
+++ ES8-object-values.js
@@ -0,0 +1,8 @@
+const countries = {
+    USA: 'United States of America',
+    IND: 'India',
+    AUS: 'Australia'
+};
+
+console.log(Object.values(countries)); // [ 'United States of America', 'India', 'Australia' ]
+// Object.values() returns an array of values of the object 
