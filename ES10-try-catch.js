764d81decd2f9383ce7f270db711d56d3565bbdf
Ernestoc14 <descreed34@hotmail.com>
2023-04-16 22:53:02 -0500

    Try Catch Class

---
+++ ES10-try-catch.js
@@ -0,0 +1,12 @@
+try{
+    anotherFunction()
+} catch (e) {
+    console.log("Error");
+}   
+
+try {
+    console.log('lagarto');
+} catch(e) {
+    console.log('F por el hello()!')
+    console.log(e);
+}
